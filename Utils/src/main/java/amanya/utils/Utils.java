776f73fddec8b5e2cbdd976833711802865a45e7
AmanyaRicky <amanyarickie@gmail.com>
2025-07-05 14:15:36 +0300

    Add files via upload

---
+++ Utils/src/main/java/amanya/utils/Utils.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package amanya.utils;
+
+/**
+ *
+ * @author user
+ */
+public class Utils {
+ public static double calculateBMI(double weightKg, double heightM) {
+        return weightKg / (heightM * heightM);
+    }
+}
+    
+    
+
